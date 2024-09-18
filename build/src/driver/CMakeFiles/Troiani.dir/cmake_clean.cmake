64910582a1f2f3f934580a3751303b8b1815e265
Walter Jose Troiani Vargas <waltertheshadow333@gmail.com>
2024-09-18 13:10:36 +0200

    finally compiles, cli working

---
+++ build/src/driver/CMakeFiles/Troiani.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Troiani.dir/main.cpp.o"
+  "CMakeFiles/Troiani.dir/main.cpp.o.d"
+  "Troiani"
+  "Troiani.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Troiani.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
